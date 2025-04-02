8b7e36d65a32400e1a31116c354e2bc3c4feedaf
Panagitis Alexandro Tsilfidis <tsilfidis@gmail.com>
2025-04-02 14:43:57 -0400

    chore: responsiveness adjustment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <div className="flex items-center p-2">
+      <div className="flex flex-col items-center p-2 lg:flex-row">
         <div className="min-w-[300px]">
           <Image
             src="/ufms.png"
@@ -13,19 +13,19 @@ export default function Home() {
             priority={true}
           />
         </div>
-        <div className="font-extrabold w-full text-center text-2xl font-[family-name:var(--font-geist-sans)]">
+        <div className="mt-8 font-extrabold w-full text-center text-2xl font-[family-name:var(--font-geist-sans)]">
           Consulta Diretor FAALC Exercício: 2025-2029
         </div>
-        <div className="min-w-[300px] sm:none"></div>
+        <div className="min-w-[300px] hidden lg:block"></div>
       </div>
       <div className="bg-foreground text-background font-semibold text-center text-2xl font-[family-name:var(--font-geist-sans)] py-4">
         Selecione sua respectiva urna
       </div>
-      <div className="mt-24 items-center justify-items-center p-2 gap-16 sm:p-8 text-2xl font-[family-name:var(--font-geist-sans)]">
-        <div className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-          <div className="flex gap-16 items-center flex-col text-2xl sm:flex-row">
+      <div className="mt-6 items-center justify-items-center p-2 gap-16 sm:p-8 text-2xl lg:mt-16 font-[family-name:var(--font-geist-sans)]">
+        <div className="flex flex-col gap-[32px] row-start-2 items-center">
+          <div className="flex gap-8 items-center flex-col text-2xl lg:flex-row">
             <a
-              className="flex items-center justify-center gap-2 min-w-[350px] min-h-[350px] rounded-lg border-2 border-solid border-foreground transition-colors hover:bg-foreground hover:text-background font-semibold sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+              className="max-w-[250px] min-h-[150px] text-center flex items-center justify-center gap-2 rounded-lg border-2 border-solid border-foreground transition-colors hover:bg-foreground hover:text-background font-semibold p-4 md:min-w-[300px] lg:min-h-[300px]"
               href="https://urnas.ufms.br/helios/e/FAALC-DOCENTES-CONSULTA-DIRETOR_9"
               target="_blank"
               rel="noopener noreferrer"
@@ -40,7 +40,7 @@ export default function Home() {
               Urna Docentes
             </a>
             <a
-              className="min-w-[350px] min-h-[350px] rounded-lg border-2 border-solid border-foreground transition-colors flex items-center justify-center gap-2 hover:bg-foreground hover:text-background font-semibold sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+              className="max-w-[250px] min-h-[150px] text-center rounded-lg border-2 border-solid border-foreground transition-colors flex items-center justify-center gap-2 hover:bg-foreground hover:text-background font-semibold p-4 md:min-w-[300px] lg:min-h-[300px]"
               href="https://urnas.ufms.br/helios/e/FAALC-TECNICOS-CONSULTA-DIRETOR_9"
               target="_blank"
               rel="noopener noreferrer"
@@ -55,7 +55,7 @@ export default function Home() {
               Urna Técnicos Administrativos
             </a>
             <a
-              className="min-w-[350px] min-h-[350px] rounded-lg border-2 border-solid border-foreground transition-colors flex items-center justify-center gap-2 hover:bg-foreground hover:text-background font-semibold sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+              className="min-w-[250px] min-h-[150px] text-center rounded-lg border-2 border-solid border-foreground transition-colors flex items-center justify-center gap-2 hover:bg-foreground hover:text-background font-semibold p-4 md:min-w-[300px] lg:min-h-[300px]"
               href="https://urnas.ufms.br/helios/e/FAALC-ESTUDANTES-CONSULTA-DIRETOR_9"
               target="_blank"
               rel="noopener noreferrer"
